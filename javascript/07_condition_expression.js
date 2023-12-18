c7c10af2a2479fedefc0efe6c35f3b816a8da586
NIXX <nikhilesh899@gmail.com>
2023-12-18 23:12:15 +0530

    18nov2023

---
+++ javascript/07_condition_expression.js
@@ -0,0 +1,43 @@
+const a = 50;
+
+/*
+
+if(expression){
+    execute this one if expression is true
+}else{
+    execute this scope if expression is false
+}
+
+*/
+
+// if-else
+
+// if(a > 70){
+//     console.log("Yes, a is greater than 70")
+// }else if(a > 40){
+//     console.log("Yes, a is greater than 40")
+// }else{
+//     console.log("Yes, a is noy greater than 40")
+// }
+
+// switch
+
+// const b = 12;
+
+// switch(b){
+//     case 45:
+//         console.log("b is equal to 45");
+//         break;
+//     case 25:
+//         console.log("b is equal to 25");
+//         break;
+//     default:
+//         console.log("this is default statement");
+//         break;
+// }
+
+// Ternery Operator
+
+// expression ? (statement if exp is true) : (statement if exp is false)
+
+(a > 55) ? console.log("Yes, it is true") : console.log("Nope, it is false")
---
+++ javascript/08_string_method.js
@@ -0,0 +1,50 @@
+// const firstName = "Nikhilesh"
+// const lastName = " Sharma"
+
+// const age = 18
+
+// const para = "The age of " + firstName + lastName + " is " + age
+
+// // String interpolation
+// const para1 = `The age of ${firstName} ${lastName} is ${age}`
+
+// console.log(para1);
+
+// const company = "Debugger";
+// console.log(company)
+
+const companyName = new String("Debugger Solution Here");
+// const companyLastName = new String("Solution");
+
+// console.log(companyName)
+
+// console.log(companyName.length)
+// console.log(companyName.toUpperCase())
+
+// console.log(companyName.toLowerCase());
+// console.log(companyName.concat(" ", companyLastName));
+
+// console.log(companyName.charAt(3));
+
+// console.log(companyName.indexOf("u"));
+
+// console.log(companyName.substring(0, 5));
+
+// console.log(companyName.slice(-5));
+
+// console.log(companyName.toLowerCase());
+// console.log(companyName.trim());
+// console.log(companyName.trimEnd());
+
+// console.log(companyName.replace("Solution", "XYZ"));
+
+console.log(companyName.split(" "));
+console.log(companyName);
+
+// You have a string "Debugger Solution", please find if "Solution" word exist in this stirng or not
+
+// Learn about all String methods
+
+// Number and its methods, Math
+
+// Date
