c32f3f5e8631b6dcbf9ff4eeea13a2991cb11c22
NIXX <nikhilesh899@gmail.com>
2023-12-30 01:26:51 +0530

    reduce and dom

--- javascript/02_basics/02_iteration.js
+++ javascript/02_basics/02_iteration.js
@@ -5,7 +5,7 @@
 // 2. It gives value if the return is true
 // 3. It return all the true values
 
-const numArr = [1,2,3,4,5,6,7,8,9,10]
+const numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 // numArr.filter(function(){
 
@@ -32,12 +32,52 @@ const filterNum = numArr.find((num) => {
 // 1. It will always return a value
 // 2. Can modify the iteration
 
-const numArr1 = [1,2,3,4,5,6,7,8,9,10]
+const numArr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 const mapValues = numArr1.map((num) => {
     return num * 10
 })
 
-console.log(mapValues);
+// console.log(mapValues);
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+];
+
+const filteredBook = books.filter((book) => book.publish < 2000 && book.edition < 2000)
+
+// console.log(filteredBook);
+
+const numArr2 = [2,4,6,8,10]
+
+const evenValues = numArr2.map((evenNum) => {
+    return evenNum * 10
+})
+
+const evenValuesForeach = numArr2.forEach((evenNum) => {
+    return evenNum * 10
+})
+
+// console.log(evenValues);
+// console.log(evenValuesForeach);
 
 // Reduce
+// Add all numbers from array
+const array1 = [1, 2, 3, 4];
+
+const reduceValue = array1.reduce((acc, currValue) => {
+    // console.log(acc);
+    // console.log(currValue);
+    return acc + currValue
+}, 0)
+
+// console.log(reduceValue);
+
