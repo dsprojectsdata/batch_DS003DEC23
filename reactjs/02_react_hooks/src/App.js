1ef304e5535d11708238b868615ea3ccf72d55c6
dsprojectsdata <85104779+dsprojectsdata@users.noreply.github.com>
2024-01-24 16:26:04 +0000

    useState hook

---
+++ reactjs/02_react_hooks/src/App.js
@@ -0,0 +1,46 @@
+import { useState } from "react"
+import './App.css';
+
+
+
+function App() {
+
+	let [count, setCount] = useState(5);
+	// const countData = useState(5);
+
+	// console.log("countData >>", countData);
+
+	// let count = 5;
+
+	const incrementNum = () => {
+		console.log("Increment Number", count);
+		count = count + 1;
+		setCount(count)
+		// document.getElementById("count-number").innerText = count
+	}
+
+	const decrementNum = () => {
+		if(count > 0){
+			setCount(count - 1)
+		}
+	}
+
+	const idName = "heading";
+
+	return (
+		<div>
+			<header>Header count - {count}</header>
+			<h3 id={idName}>Increment/Decrement Number</h3>
+			<h4>Count - <span id='count-number'>{count}</span> </h4>
+			<div>
+				<button onClick={incrementNum}>INC - {count}</button> &nbsp;
+				<button onClick={decrementNum}>DEC - {count}</button>
+			</div>
+		</div>
+	);
+
+}
+
+export default App;
+
+// {} -> evaluate expression
