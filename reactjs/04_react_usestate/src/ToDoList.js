b2a135a441bbea6b3cd3069a4e4c6c2d967ce66d
dsprojectsdata <85104779+dsprojectsdata@users.noreply.github.com>
2024-02-05 17:05:27 +0000

    to do list

--- reactjs/04_react_usestate/src/ToDoList.js
+++ reactjs/04_react_usestate/src/ToDoList.js
@@ -1,12 +1,51 @@
+import { useState } from "react"
 
+let incNum = 1;
 
 const ToDoList = () => {
+
+    const [inputVal, setInputVal] = useState("");
+    const [allToDoList, setAllToDoList] = useState([]);
+
+    const handleInputChange = (event) => {
+        setInputVal(event.target.value);
+    }
+
+    const addToDoTask = () => {
+        // console.log("on clicking the button >>",inputVal)
+        const taskObj = {
+            id: incNum,
+            task: inputVal
+        }
+        const newTaskArr = [...allToDoList, taskObj];
+        setAllToDoList(newTaskArr)
+        incNum++;
+    }
+
+    const handleDelete = (id) => {
+        console.log("allToDoList >>", allToDoList)
+        console.log("id >>", id)
+
+        const filteredArr = allToDoList.filter((task) => task.id !== id)
+        // console.log("filteredArr >>",filteredArr);
+        setAllToDoList(filteredArr);
+    }
+
+    // console.log("allToDoList >>", allToDoList)
+
     return (
         <>
-            <input /> <button>Add Task</button>
+            <input onChange={handleInputChange} /> <button onClick={addToDoTask} >Add Task</button>
+
             <div>
                 <ul>
-                    <li>Task 1</li>
+                    {/* <li>Task 1 <button>Del</button> </li>
+                    <li>Task 2 <button>Del</button></li> */}
+                    {allToDoList.map((task) => {
+                        return (
+                            <li>{task.task} <button onClick={() => handleDelete(task.id)}>Del</button> </li>
+                        )
+                    })}
                 </ul>
             </div>
         </>
@@ -14,3 +53,12 @@ const ToDoList = () => {
 }
 
 export default ToDoList;
+
+// [
+//     {
+//         task: "Task 1"
+//     },
+//     {
+//         task: "Task 2"
+//     }
+// ]
